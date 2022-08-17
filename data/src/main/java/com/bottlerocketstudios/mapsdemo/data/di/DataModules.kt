bf7d2b3d0d90e42a715da06d42e8f67902dad1ca
Josh Gebbeken <josh.gebbeken@bottlerocketstudios.com>
2022-08-17 13:20:51 -0500

    feature/ASAA-18-Yelp added viewmodels in modules

--- data/src/main/java/com/bottlerocketstudios/mapsdemo/data/di/DataModules.kt
+++ data/src/main/java/com/bottlerocketstudios/mapsdemo/data/di/DataModules.kt
@@ -6,11 +6,14 @@ import com.bottlerocketstudios.mapsdemo.data.network.YelpService
 import com.bottlerocketstudios.mapsdemo.data.network.YelpServiceFactory
 import com.bottlerocketstudios.mapsdemo.data.serialization.DateTimeAdapter
 import com.bottlerocketstudios.mapsdemo.data.serialization.ProtectedPropertyAdapter
+import com.bottlerocketstudios.mapsdemo.data.viewmodels.YelpViewModel
 import com.bottlerocketstudios.mapsdemo.domain.infrastructure.coroutine.DispatcherProvider
 import com.bottlerocketstudios.mapsdemo.domain.repositories.YelpRepository
 import com.bottlerocketstudios.mapsdemo.infrastructure.coroutine.DispatcherProviderImplementation
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 
 import org.koin.dsl.module
@@ -28,5 +31,11 @@ object DataModule {
 object NetworkModule {
     val module = module {
         single { YelpServiceFactory().produce() }
+    }
+}
+
+object ViewModelModule {
+    val module = module {
+        viewModel { YelpViewModel() }
     }
 }
