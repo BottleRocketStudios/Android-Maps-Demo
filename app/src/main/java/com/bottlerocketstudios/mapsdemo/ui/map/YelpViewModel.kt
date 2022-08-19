051ea06b54c3aabef5fac2bc12ce94cd97ed2536
Josh Gebbeken <josh.gebbeken@bottlerocketstudios.com>
2022-08-19 10:13:03 -0500

    feature/ASAA-18-Yelp fix interceptor and lat lng of dallas

--- app/src/main/java/com/bottlerocketstudios/mapsdemo/ui/map/YelpViewModel.kt
+++ app/src/main/java/com/bottlerocketstudios/mapsdemo/ui/map/YelpViewModel.kt
@@ -19,7 +19,7 @@ class YelpViewModel: BaseViewModel() {
     val yelpBusinessState: StateFlow<List<Business>> = _yelpBusinessState
 
     init {
-        getYelpBusinesses(YelpLatLngSearch(32.7767, 96.7970))
+        getYelpBusinesses(YelpLatLngSearch(32.7767, -96.7970))
     }
 
     fun getYelpBusinesses(yelpLatLngSearch: YelpLatLngSearch) {
