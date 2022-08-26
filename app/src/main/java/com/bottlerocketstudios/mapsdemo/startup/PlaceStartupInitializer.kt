9b120179902dd0409fdb6cdfb699aafcf369cd27
Jared Stockton <jared.stockton@bottlerocketstudios.com>
2022-08-26 12:27:28 -0500

    AASA-18 Review commit: Move `PlaceStartupInitializer` implementation to `:app` with a dependency on `KoinStartupInitializer`\
    * Create empty `BasePlacesStartupInitializer` in `places` module that the actual implementation, `PlaceStartupInitializer`, extends from as a breadcrumb to its existence.

--- app/src/main/java/com/bottlerocketstudios/mapsdemo/startup/PlaceStartupInitializer.kt
+++ app/src/main/java/com/bottlerocketstudios/mapsdemo/startup/PlaceStartupInitializer.kt
@@ -1,12 +1,13 @@
-package com.bottlerocketstudios.places.startup
+package com.bottlerocketstudios.mapsdemo.startup
 
 import android.content.Context
 import androidx.startup.Initializer
 import com.bottlerocketstudios.mapsdemo.domain.di.BuildConfigProvider
+import com.bottlerocketstudios.places.startup.BasePlacesStartupInitializer
 import com.google.android.libraries.places.api.Places
 import org.koin.java.KoinJavaComponent.inject
 
-class PlacesStartupInitializer : Initializer<Unit> {
+class PlacesStartupInitializer : BasePlacesStartupInitializer {
     override fun create(context: Context) {
         val buildConfigProvider by inject<BuildConfigProvider>(BuildConfigProvider::class.java)
         // Secrets Gradle Plugin is being used in the project. To give Places the API Key, the BuildConfigProvider is being injected here
@@ -14,5 +15,5 @@ class PlacesStartupInitializer : Initializer<Unit> {
         Places.initialize(context, buildConfigProvider.mapsApiKey)
     }
 
-    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+    override fun dependencies(): List<Class<out Initializer<*>>> = listOf(KoinStartupInitializer::class.java)
 }
