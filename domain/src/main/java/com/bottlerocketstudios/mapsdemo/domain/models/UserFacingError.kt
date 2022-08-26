b08c52a8f4699e4a551e4c35c20a58691d4996a4
Josh Gebbeken <josh.gebbeken@bottlerocketstudios.com>
2022-08-26 11:01:00 -0500

    feature/ASAA-18-Yelp PR feedback change

--- domain/src/main/java/com/bottlerocketstudios/mapsdemo/domain/models/UserFacingError.kt
+++ domain/src/main/java/com/bottlerocketstudios/mapsdemo/domain/models/UserFacingError.kt
@@ -4,8 +4,8 @@ import androidx.annotation.StringRes
 
 sealed class UserFacingError {
     data class GeneralError(
-        val title: Int,
-        val description: Int
+        @StringRes val title: Int,
+        @StringRes val description: Int
     ) : UserFacingError()
     data class ApiError(
         val code: Int = 0,
